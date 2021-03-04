924888100cd86abb96d750af8a5d4990ee9397f1
Hayoung Gam <hahhyoung@gmail.com>
2021-03-04 14:29:23 +0900

    Add canvas context js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#35393c"; // 선의 색과 스타일
+ctx.lineWidth = 2.5; // 선의 굵기
 
 let painting = false;
 
@@ -6,10 +10,24 @@ function stopPainting() {
   painting = false;
 }
 
+function startPainting() {
+  painting = true;
+}
+
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(x, y);
+  if (!painting) {
+    // 그림을 그리고 있지 않을 때
+    ctx.beginPath();
+    ctx.moveTo(x, y); // 선을 그리는 시작점
+    console.log('aaaa')
+  } else {
+    // 그림을 그리고 있을 때
+    ctx.lineTo(x, y); // 선을 그리는 마지막 지점
+    ctx.stroke(); // moveTo와 lineTo로 경로가 정해진 지점에서 선을 그리는 역할을 수행
+    console.log('bbb')
+  }
 }
 
 function onMouseDown(event) {
@@ -17,13 +35,9 @@ function onMouseDown(event) {
   console.log(painting);
 }
 
-function onMouseUp(event) {
-  stopPainting();
-}
-
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }

30379e5c69e1ca0191c3489ab2133e25efecd9a0
Hayoung Gam <hahhyoung@gmail.com>
2021-03-04 17:15:34 +0900

    Save image

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
-const mode = document.getElementById("jsMode");
+const modeBtn = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#35393c";
 const CANVAS_SIZE = 700;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "#fff";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 
 ctx.strokeStyle = INITIAL_COLOR; // 선의 색과 스타일
 ctx.fillStyle = INITIAL_COLOR; // 면의 색과 스타일
@@ -21,8 +25,10 @@ function stopPainting() {
   painting = false;
 }
 
-function startPainting() {
-  painting = true;
+function startPainting(event) {
+  if (filling === false) {
+    painting = true;
+  }
 }
 
 function onMouseMove(event) {
@@ -32,12 +38,10 @@ function onMouseMove(event) {
     // 그림을 그리고 있지 않을 때. 경로를 만든다
     ctx.beginPath(); // 경로 생성
     ctx.moveTo(x, y); // 선을 그리는 시작점 좌표
-    console.log('aaaa')
   } else {
     // 그림을 그리고 있을 때. 그린다
     ctx.lineTo(x, y); // 선을 그리는 마지막 지점 좌표
     ctx.stroke(); // moveTo와 lineTo로 경로가 정해진 지점에서 선을 그리는 역할을 수행
-    console.log('bbb')
   }
 }
 
@@ -56,11 +60,11 @@ function handleModeClick() {
   if (filling === true) {
     // 현재는 선 그리기 모드
     filling = false;
-    mode.innerText = "Fill"
+    modeBtn.innerText = "Fill"
   } else {
     // 현재는 필 모드
     filling = true;
-    mode.innerText = "Paint"
+    modeBtn.innerText = "Paint"
   }
 }
 
@@ -70,12 +74,26 @@ function handleCanvasClick() {
   }
 }
 
+function handleCM(event) {
+  event.preventDefault();
+}
+
+function handleSaveClick() {
+  // image변수: 캔버스에 그린 내용을 데이터URL로 취득함
+  const image = canvas.toDataURL("image/png", 0.7);
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS[EXPORT]"; // 다운로드 디폴트 파일명
+  link.click();
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
 }
 
 // 팔레트의 각각의 색상을 가져옴
@@ -85,6 +103,10 @@ if (range) {
   range.addEventListener("input", handleRangeChange);
 }
 
-if (mode) {
-  mode.addEventListener("click", handleModeClick);
+if (modeBtn) {
+  modeBtn.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }

b8d4b6c6e132da913417b8133f21e1948c7c33be
Hayoung Gam <hahhyoung@gmail.com>
2021-03-04 14:41:26 +0900

    Add canvas size

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
+canvas.width = 700;
+canvas.height = 700;
+
 ctx.strokeStyle = "#35393c"; // 선의 색과 스타일
 ctx.lineWidth = 2.5; // 선의 굵기
 
@@ -18,13 +21,13 @@ function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
-    // 그림을 그리고 있지 않을 때
-    ctx.beginPath();
-    ctx.moveTo(x, y); // 선을 그리는 시작점
+    // 그림을 그리고 있지 않을 때. 경로를 만든다
+    ctx.beginPath(); // 경로 생성
+    ctx.moveTo(x, y); // 선을 그리는 시작점 좌표
     console.log('aaaa')
   } else {
-    // 그림을 그리고 있을 때
-    ctx.lineTo(x, y); // 선을 그리는 마지막 지점
+    // 그림을 그리고 있을 때. 그린다
+    ctx.lineTo(x, y); // 선을 그리는 마지막 지점 좌표
     ctx.stroke(); // moveTo와 lineTo로 경로가 정해진 지점에서 선을 그리는 역할을 수행
     console.log('bbb')
   }
